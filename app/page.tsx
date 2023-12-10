929bc63ae4ce86e68d944d02c1f062f053f51fa8
Arghun <arghava.mousanezhad@utopiamusic.com>
2023-12-10 11:42:10 +0100

    Send Email using Next.js Server Actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import GithubAccessTokenEmail from "@/components/email-template";
+import { Resend } from "resend";
+
+export default async function Home() {
+  async function send() {
+    "use server";
+
+    const resend = new Resend();
+
+    const { data } = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: ["arghun.developer@gmail.com"],
+      subject: "First Resend Email",
+      react: GithubAccessTokenEmail({ username: "Sara" }),
+    });
+
+    console.log(data);
+  }
+
+  return (
+    <form action={send}>
+      <button
+        type="submit"
+        className="bg-gray-300 p-2 rounded-md cursor-pointer m-4"
+      >
+        Send Email
+      </button>
+    </form>
+  );
 }
